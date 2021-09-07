427129fe94172244258d810f4246df1fc7c2973a
81244412 <bm9928@student.uni-lj.si>
2021-09-07 20:12:34 +0200

    upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,85 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'naloga6';
+  opened!: boolean;
+  screensize!: number;
+  smallerScreen: boolean = false;
+  //itemsArray: any[] | undefined;
+  itemsArray= [1,1,1,1,1,1,1,1,1];
+  itemsArray1= [1,1,1,1];
+  scrollYCurrent: number = 0;
+  selected = 'none';
+  gridColumns = 3;
+  filtered: boolean = false;
+
+
+  goToError() {
+    alert("Not finished yet, because it's just a dummy website! :)");
+  }
+
+
+  toggleGridColumns() {
+    this.gridColumns = this.gridColumns === 3 ? 4 : 3;
+  }
+
+  filterChange(event: any) {
+    //console.log(event);
+    if (event.value === "price") {
+      this.filtered = true;
+      this.itemsArray= [1,1,1,1,1,1,1,1,1];
+    } else if (event.value === "none") {
+      this.filtered = false;
+      this.itemsArray= [1,1,1,1,1,1,1,1,1];
+    }
+  }
+
+
+  @HostListener('window:resize', ['$event'])
+    onResize(event: any) {
+    this.screensize = window.innerWidth;
+    //console.log(window.innerWidth);
+    if (this.screensize < 1000) {
+      this.smallerScreen = true;
+    } else {
+      this.smallerScreen = false;
+    }
 }
+
+
+@HostListener('window:scroll', ['$event']) 
+onScroll(event: any) {
+  //console.log(window.innerHeight + window.pageYOffset);
+  //console.log(document.body.scrollHeight);
+
+    if (window.innerHeight + window.pageYOffset >= document.body.scrollHeight-50) {
+      //console.log("end");
+      this.itemsArray.push(1,1,1);
+    }
+}
+
+
+changeHeight() {
+  let element = document.getElementById("toolbar");
+  if (element !== null && element.classList.contains("bigger")) {
+    element.classList.remove("bigger");
+  } else if (element !== null) {
+    element.classList.add("bigger");
+  }
+}
+
+ngOnInit() {
+  
+//console.log(this.itemsArray);
+
+
+  this.screensize = window.innerWidth;
+  if (this.screensize < 1000) {
+    this.smallerScreen = true;
+  } else {
+    this.smallerScreen = false;
+  }
+}
+  
+}
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,38 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatGridListModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatSelectModule,
+
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
